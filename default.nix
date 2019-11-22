58979c984a437746c1a4047a3702117edf3d42f0
Jonathan Reeve <jon.reeve@gmail.com>
2019-11-22 15:28:49 -0500

    update deps

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, HandsomeSoup, hxt, stdenv }:
+{ mkDerivation, base, HandsomeSoup, hxt, pandoc, stdenv }:
 mkDerivation {
   pname = "html2tei";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base HandsomeSoup hxt ];
+  executableHaskellDepends = [ base HandsomeSoup hxt pandoc ];
   description = "Transform Project Gutenberg books from HTML to TEI XML";
   license = stdenv.lib.licenses.lgpl21;
 }
